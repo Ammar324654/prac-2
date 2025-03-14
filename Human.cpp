52f99b72ef1de5d09f751f7a0676e99ccbc3e305
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-14 20:46:56 +1030

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -1,19 +1,27 @@
 #include "Human.h"
-#include "Monkey.h"
-#include "Robot.h"
-#include "Pirate.h"
-#include "Ninja.h"
-#include "Zombie.h"
+#include <iostream>
+#include <memory>
+using namespace std;
 
 Move* Human::makeMove() {
-    string moveInput;
-    cout << "Enter Move: ";
-    cin >> moveInput;
+    string moveChoice;
+    cout << "Enter Move (Monkey, Robot, Pirate, Ninja, Zombie): ";
+    cin >> moveChoice;
 
-    if (moveInput == "Monkey") return new Monkey();
-    if (moveInput == "Robot") return new Robot();
-    if (moveInput == "Pirate") return new Pirate();
-    if (moveInput == "Ninja") return new Ninja();
-    if (moveInput == "Zombie") return new Zombie();
+    if (moveChoice == "Monkey") {
+        return new Monkey();
+    } else if (moveChoice == "Robot") {
+        return new Robot();
+    } else if (moveChoice == "Pirate") {
+        return new Pirate();
+    } else if (moveChoice == "Ninja") {
+        return new Ninja();
+    } else if (moveChoice == "Zombie") {
+        return new Zombie();
+    }
+    return nullptr;
+}
 
-    return nullptr;  
+string Human::getName() {
+    return "Human";
+}
