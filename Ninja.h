01f3058f46247c61856d86bb96fe620eb1574b64
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-14 18:18:32 +1030

    Create Ninja.h

---
+++ Ninja.h
@@ -0,0 +1,14 @@
+#ifndef NINJA_H
+#define NINJA_H
+
+#include "Move.h"
+
+class Ninja : public Move {
+public:
+    string getName() override { return "Ninja"; }
+    bool beats(Move* other) override {
+        return other->getName() == "Pirate" || other->getName() == "Zombie";
+    }
+};
+
+#endif
