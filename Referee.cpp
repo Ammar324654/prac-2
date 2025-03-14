12b753c9af23f6fb387d946c204646ecf2726c3b
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-14 20:47:16 +1030

    Update Referee.cpp

--- Referee.cpp
+++ Referee.cpp
@@ -1,16 +1,16 @@
 #include "Referee.h"
+#include "Move.h"
+
+Referee::Referee() {}
 
 Player* Referee::refGame(Player* player1, Player* player2) {
     Move* move1 = player1->makeMove();
     Move* move2 = player2->makeMove();
 
-    if (move1->getName() == move2->getName()) {
-        return nullptr; // tie
-    }
-
     if (move1->beats(move2)) {
-        return player1;  // Player 1 wins
-    } else {
-        return player2;  // Player 2 wins
+        return player1;
+    } else if (move2->beats(move1)) {
+        return player2;
     }
+    return nullptr;  // In case of a tie
 }
