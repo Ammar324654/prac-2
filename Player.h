90c029c73bb4e27b016b51b424c2d891d7cd1d20
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-14 20:46:25 +1030

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,13 +1,14 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
 #include "Move.h"
 
 class Player {
 public:
-    virtual Move* makeMove() = 0;  // Returns a Move object
-    virtual string getName() = 0;  // Returns the player's name
-    virtual ~Player() {}           // Virtual destructor
+    virtual Move* makeMove() = 0;
+    virtual string getName() = 0;
+    virtual ~Player() {}
 };
 
-#endif 
+#endif
